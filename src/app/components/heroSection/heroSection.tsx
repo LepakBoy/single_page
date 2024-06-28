3f4080c90cbe6dec24fb8089c7ccf49259c937cb
fikrinadzif <fikrinadzif35@gmail.com>
2024-06-28 14:21:22 +0700

    add svg, improve hero section responsive

--- src/app/components/heroSection/heroSection.tsx
+++ src/app/components/heroSection/heroSection.tsx
@@ -1,11 +1,23 @@
 import React from 'react';
 import styles from './heroSection.module.css';
 import Image from 'next/image';
+import DownloadIcon from '../../../../public/download.svg';
+import TwitterIcon from '../../../../public/twitter.svg';
+import LinkedInIcon from '../../../../public/linkedin.svg';
+import GithubIcon from '../../../../public/github.svg';
+import WebIcon from '../../../../public/web.svg';
+import Link from 'next/link';
 
 export default function HeroSection() {
-  const listButton = ['twitter', 'web', 'linkedin', 'github'];
+  const listButton = [
+    { name: 'twitter', svg: TwitterIcon },
+    { name: 'web', svg: WebIcon },
+    { name: 'linkedin', svg: LinkedInIcon },
+    { name: 'github', svg: GithubIcon },
+  ];
+
   return (
-    <div className={styles.container}>
+    <section className={styles.container}>
       <div className={styles.content}>
         <h2 className={styles.title_name}>I am Fikri Ahmad</h2>
         <h1 className={styles.title}>
@@ -13,7 +25,7 @@ export default function HeroSection() {
         </h1>
         <Image
           className={styles.photo_mobile}
-          src="/photo.jpg"
+          src="/personal.jpeg"
           height={300}
           width={300}
           alt=""
@@ -24,28 +36,46 @@ export default function HeroSection() {
           perferendis laborum quam voluptas debitis
         </p>
         <div className={styles.buttons}>
-          <button className={`${styles.button_cv} ${styles.button_effect}`}>
+          <Link
+            href={'/'}
+            className={`${styles.button_cv} ${styles.button_effect}`}
+          >
             Download CV
-          </button>
-          {listButton.map((button) => (
-            <button
-              key={button}
-              className={`${styles.button_media} ${styles.button_effect}`}
-            >
-              {button}
-            </button>
-          ))}
+            <Image
+              src={DownloadIcon}
+              width={24}
+              height={24}
+              alt="download"
+            />
+            {/* <DownloadIcon /> */}
+          </Link>
+          <ul className={styles.social_box}>
+            {listButton.map((button) => (
+              <li
+                key={button.name}
+                className={`${styles.button_media} ${styles.button_effect}`}
+              >
+                <Image
+                  className={styles.svg_icon}
+                  src={button.svg}
+                  width={24}
+                  height={24}
+                  alt=""
+                />
+              </li>
+            ))}
+          </ul>
         </div>
       </div>
       <div className={styles.hero_image}>
         <Image
           className={styles.photo}
-          src="/photo.jpg"
+          src="/personal.jpeg"
           height={300}
           width={300}
           alt=""
         />
       </div>
-    </div>
+    </section>
   );
 }

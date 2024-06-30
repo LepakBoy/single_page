1f4bf51ccc43da755e3f9d14e066357888abe0b5
fikrinadzif <fikrinadzif35@gmail.com>
2024-06-30 17:54:12 +0700

    init skills section

--- src/app/components/skillSection/skillSection.tsx
+++ src/app/components/skillSection/skillSection.tsx
@@ -1,5 +1,16 @@
 import React from 'react';
+import styles from './skillSection.module.css';
+import TitleSection from '../molecule/titleSection/titleSection';
+import DescriptionSection from '../molecule/descriptionSection/descriptionSection';
 
 export default function SkillSection() {
-  return <div>skillSection</div>;
+  return (
+    <div className={styles.container}>
+      <TitleSection
+        centered
+        title="My Skills"
+      />
+      <DescriptionSection text="We put your ideas and thus your wishes in the form of a unique web project that inspires you and you customers." />
+    </div>
+  );
 }
---
+++ src/app/components/skillSection/skillsCard/skillsCard.tsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import styles from './skillsCard.module.css';
+
+export default function SkillsCard() {
+  return <div className={styles.container}>SkillsCard</div>;
+}

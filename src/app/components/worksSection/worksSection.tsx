966f80c196bd74390d87322f313c5ca9dfff00bb
fikrinadzif <fikrinadzif35@gmail.com>
2024-07-01 11:18:21 +0700

    delete unused code

--- src/app/components/worksSection/worksSection.tsx
+++ src/app/components/worksSection/worksSection.tsx
@@ -1,11 +1,16 @@
 import React from 'react';
 import styles from './worksSection.module.css';
 import TitleSection from '../molecule/titleSection/titleSection';
+import DescriptionSection from '../molecule/descriptionSection/descriptionSection';
 
 export default function WorksSection() {
   return (
     <section className={styles.section}>
-      <TitleSection title="My Recent Works" />
+      <TitleSection
+        centered
+        title="My Recent Works"
+      />
+      <DescriptionSection text="We put your ideas and thus your wishes in the form of a unique web project that inspires you and you customers." />
     </section>
   );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,4 @@
 import HeroSection from './components/heroSection/heroSection';
-import Navbar from './components/navbar/navbar';
 import ResumeSection from './components/resumeSection/resumeSection';
 import ServicesSection from './components/servicesSection/srevicesSection';
 import SkillSection from './components/skillSection/skillSection';
@@ -8,12 +7,11 @@ import WorksSection from './components/worksSection/worksSection';
 export default function Home() {
   return (
     <main>
-      {/* <Navbar /> */}
       <HeroSection />
       <ServicesSection />
       <ResumeSection />
       <SkillSection />
-      {/* <WorksSection /> */}
+      <WorksSection />
     </main>
   );
 }
